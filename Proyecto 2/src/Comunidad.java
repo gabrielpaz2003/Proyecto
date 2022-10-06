5c53c2e4295d13f2fb7170d2fd2d7f45a5e378d7
sharlisbg <108887285+sharlisbg@users.noreply.github.com>
2022-10-06 09:46:15 -0600

    Update Comunidad.java
    Cambios adicionales a clase Comunidad

--- Proyecto 2/src/Comunidad.java
+++ Proyecto 2/src/Comunidad.java
@@ -1,8 +1,8 @@
-public class Comunidad  { //creamos la clase perro
+public class Comunidad  { //creamos la clase Comunidad
     private int personas;
     private String nombre;
     private ArrayList<Familia> familias;
-    private int[] drenajes = new int[3]; 
+    private ArrayList<drenaje> drenajes; 
 
 	public Comunidad() {
         nombre = "";
@@ -17,29 +17,84 @@ public class Comunidad  { //creamos la clase perro
 		nombre = nom;
 		personas = per;
 	}
-
+        
+	/** 
+	 * @return int
+	 */
 	public int getPersonas() {
 		return this.personas;
 	}
-
+	
+	/** 
+	 * @param personas
+	 */
 	public void setPersonas(int personas) {
 		this.personas = personas;
 	}
-
+	
+	/** 
+	 * @return String
+	 */
 	public String getNombre() {
 		return this.nombre;
 	}
-
+	
+	/** 
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	
+	/** 
+	 * @param capacidad
+	 * @param posicion
+	 */
 	public void setDrenajes(int capacidad, int posicion){
         drenajes[posicion] = capacidad;
     }
 
+	/** 
+	* @param posicion
+	* @return int
+	*/
+	public int getDrenajes(int posicion){
+		return drenajes[posicion];
+	}    
 
-    public int getDrenajes(int posicion){
-        return drenajes[posicion];
-    }    
+	 /*
+	 * Calcular la vida útil de drenaje con base a los mantenimientos
+	 */
+	public String vidaUtilDrenajes(){
+		// Con la cantidad de mantimientos
+		int mantenimientos = 2;//cantMantenimientos(); 
+		int vidaUtilPromedio = 10; 
+		float cambios = mantenimientos*100/vidaUtilPromedio;
+		if(cambios >= 75 ){
+			String mensaje = "La vida útil está en un 75%";
+			return mensaje;}
+		if(cambios < 75 && cambios >= 50 ){
+			String mensaje = "La vida útil está en un 50%";
+			return mensaje;}
+		if(cambios < 50 && cambios >= 25 ){
+			String mensaje = "La vida útil está en un 25%\nEste es un estado de Alerta. ";
+			return mensaje;}
+		if(cambios < 25){
+			String mensaje = "La vida útil está llegando a su fin.\nEn los siguientes meses debe considerar cambiar o dar un mantenimeinto fuerte al sistema";	
+			return mensaje;}
+	
+	}
+	
+	/** 
+	 * @return String
+	 */
+	@Override
+	public String toString() {
+		return "{" +
+			" personas='" + getPersonas() + "'" +
+			", nombre='" + getNombre() + "'" +
+			", familias='" + getFamilias() + "'" +
+			", drenajes='" + getDrenajes() + "'" +
+			"}";
+	}
 }
